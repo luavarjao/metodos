e352c561ffa1629073fff0d2723fc23481cecb68
Luanna <lua.varjao@gmail.com>
2023-08-14 07:41:30 -0300

    Resolução Ex 03

--- 03/index.js
+++ 03/index.js
@@ -1,1 +1,12 @@
 const texto = "Aprenda programar do zero na Cubos Academy";
+
+let textoMinusculo = texto.toLowerCase();
+let textoSeparado = textoMinusculo.split(" ");
+let i = 0;
+let textoFinal = "";
+while(i < (textoSeparado.length - 1)){
+   textoFinal += textoSeparado[i] + "-";
+    i++;
+}
+textoFinal +=textoSeparado[textoSeparado.length - 1];
+console.log(textoFinal);
