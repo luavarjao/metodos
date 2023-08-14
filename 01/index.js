7fbeb3a2bfa4045b2e79d329133fa23ae28ab3e6
Luanna <lua.varjao@gmail.com>
2023-08-13 22:11:04 -0300

    Resolução Ex 01

--- 01/index.js
+++ 01/index.js
@@ -1,1 +1,7 @@
 const comentario = "Esse COVID é muito perigoso!";
+
+if (comentario.includes("COVID") || comentario.includes("pandemia") || comentario.includes("covid")){
+    console.log(`Comentário bloqueado por conter palavras proibidas`)
+} else {
+    console.log(`Comentário autorizado`);
+}
