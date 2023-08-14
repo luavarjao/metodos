a2f7a8f48b715a976f7cf6683bf9cc5f96fd6761
Luanna <lua.varjao@gmail.com>
2023-08-13 22:17:57 -0300

    Resolução Ex 02

--- 02/index.js
+++ 02/index.js
@@ -1,2 +1,17 @@
 const cpf = "12345678900";
 const cnpj = "12345678900";
+
+//-   Validar o tamanho (11 números)
+//-   Imprimir o CPF formatado se for válido
+
+if (cpf.length === 11){
+    console.log(cpf);
+} else {
+    console.log("CPF Inválido");
+}
+
+if(cnpj.length === 14){
+    console.log(cnpj);
+} else {
+    console.log("CNPJ inválido");
+}
