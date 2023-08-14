e46c076b3a95d3eac4d6c4a888073459bd723fa4
Luanna <lua.varjao@gmail.com>
2023-08-14 14:34:56 -0300

    Resolução Ex 04

--- 04/index.js
+++ 04/index.js
@@ -1,3 +1,15 @@
 let identificador = "123";
 let nome = "José silva costa";
 let email = "      jose@email.com  ";
+
+let identificadorCerto = identificador.padStart(6, "0");
+let nomeDividido= nome.split(" ");
+let nomeCorreto = "";
+for(let i of nomeDividido){
+    nomeCorreto += i[0].toUpperCase() + i.slice(1) + " ";
+}
+let nomeCompleto = nomeCorreto.trim();
+let emailCorreto = email.trim();
+console.log(identificadorCerto);
+console.log(nomeCompleto);
+console.log(emailCorreto);
