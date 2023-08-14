8667915f98974c536196e3f6c0fd111d4af4f0f1
Luanna <lua.varjao@gmail.com>
2023-08-14 19:24:16 -0300

    Resolução Ex 07

--- 07/index.js
+++ 07/index.js
@@ -1,1 +1,23 @@
-const email = "aluno@cubos.academy";
+const email = ["aluno@cubos.academy",
+`jose@cubos.academy`,
+`jose@cubos.academy.br`,
+`jose.messias@cubos.academy`,
+`jose.messias@cubos.io`,
+`jose@cubos`,
+`jose.messias@cubos`,
+`jose.messias@.`,
+`jose.@cubos`,
+`.@`,
+`@.`,
+`jose.messias@cubos.`,
+`.messias@cubos.`,
+`.messias@cubos`
+]
+
+for(let i of email){
+if(i.includes("@") && i.includes(".") && i[0]!== "." && i[i.length-1] !== "."){
+    console.log("E-mail válido");
+} else {
+    console.log("E-mail inválido");
+}
+}
