fbd35db7b9ae426801024513771aa0b96b02a6f0
Luanna <lua.varjao@gmail.com>
2023-08-14 14:51:04 -0300

    Resolução Ex 05

--- 05/index.js
+++ 05/index.js
@@ -1,1 +1,22 @@
 const numeroCartao = '1111222233334444';
+let x = 0;
+let numeroCartaoCorreto = "";
+
+for(let i of numeroCartao){
+    if(x < 4 || x > 11){
+        numeroCartaoCorreto += i;
+    } else {
+        numeroCartaoCorreto += "*";
+    }
+    x++;
+}
+
+console.log(numeroCartaoCorreto);
+
+let numeros = numeroCartao.slice(0,4)+ "********" + numeroCartao.slice(12);
+console.log(numeros);
+
+let quatroUltimos = numeroCartao.slice(12);
+let asteriscos = quatroUltimos.padStart(12,"*");
+let final = numeroCartao.slice(0,4) + asteriscos;
+console.log(final);
