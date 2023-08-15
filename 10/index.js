8b0a477c430e87a2dbdde239862a51fdc55d4d82
Luanna <lua.varjao@gmail.com>
2023-08-14 22:41:26 -0300

    Resolução Ex 10

--- 10/index.js
+++ 10/index.js
@@ -1,1 +1,10 @@
 const nomeArquivo = 'Foto da Familia.pdf';
+let extensao = nomeArquivo.slice(nomeArquivo.length-3);
+
+//-   São permitidos arquivos: jpg, jpeg, gif e png;
+
+if(extensao === "jpg" || extensao === "jpeg" || extensao === "gif" || extensao === "png"){
+    console.log("Arquivo válido");
+} else {
+    console.log("Arquivo inválido");
+}
