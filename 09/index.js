8d557f00608f948933d4e00c1ebdc793aff1437d
Luanna <lua.varjao@gmail.com>
2023-08-14 22:35:26 -0300

    Resolução Ex 09

--- 09/index.js
+++ 09/index.js
@@ -1,1 +1,16 @@
 const nome = 'Guido Cerqueira';
+
+//-   O nickname deve conter, no máximo, 13 caracteres;
+//-   Deve começar sempre com "@";
+//-   Não deve existir espaços em branco;
+//-   São permitidos apenas caracteres minúsculos.
+
+let nomes = nome.split(" ");
+let nomeJunto = "";
+for (let i of nomes){
+    nomeJunto += i
+}
+let caracteres = nomeJunto.slice(0,12);
+let minusculos = caracteres.toLowerCase();
+console.log(`@${minusculos}`);
+
