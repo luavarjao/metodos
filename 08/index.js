4d10e76278733b7db5b1616476d1485e9afc7f15
Luanna <lua.varjao@gmail.com>
2023-08-14 22:18:26 -0300

    Resolução Ex 08

--- 08/index.js
+++ 08/index.js
@@ -1,1 +1,13 @@
 const cpf = "011.022.033-44";
+let cpfSeparado = cpf.split("");
+let final = "";
+
+for(let i of cpfSeparado){
+    if(i !== "." && i !== "-"){
+        final += i;
+    }
+}
+
+
+
+console.log(final);
